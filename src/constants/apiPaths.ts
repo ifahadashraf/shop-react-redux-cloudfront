7c730197d01e9e7b9d7cd9a407533ef575cbdf73
Fahad Ashraf <fahadashraf9612@gmail.com>
2023-11-06 18:30:38 +0100

    Task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://xv9kspe13a.execute-api.eu-north-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://5svg927fyd.execute-api.eu-west-1.amazonaws.com/dev/import",
   bff: "https://xv9kspe13a.execute-api.eu-north-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
